fcf15b335d462e22a17f4f869e73921b3e8ab569
Arne Goeteyn <goeteynarne@gmail.com>
2025-08-17 18:22:20 +0200

    fix guess-indent

--- nvim/.config/nvim/lua/kickstart/plugins/lint.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -40,6 +40,7 @@ return {
       -- lint.linters_by_ft['ruby'] = nil
       -- lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft['typescriptreact'] = { 'eslint' }
       lint.linters_by_ft['dockerfile'] = nil
       lint.linters_by_ft['markdown'] = nil
       lint.linters_by_ft['go'] = { 'golangcilint' }
