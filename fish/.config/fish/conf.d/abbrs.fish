dc2a4913b627053414669b4de4c4d574eebd7b1d
Arne Goeteyn <goeteynarne@gmail.com>
2025-10-04 12:04:07 +0200

    Update fish abbreviations

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,9 +1,9 @@
 abbr -a -- ls lsd
 abbr -a -- l 'lsd -l'
+abbr -a -- la 'lsd -a'
 abbr -a -- lla 'lsd -la'
 abbr -a -- lt 'lsd --tree'
 abbr -a -- vim nvim
 abbr -a -- h 'hx .'
 abbr -a -- y yazi
 abbr -a -- stow 'stow -d $HOME/Documents/dotty -t $HOME'
-abbr -a -- la 'ls -a'
