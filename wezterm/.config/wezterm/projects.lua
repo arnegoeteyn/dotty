0579650e0e0e6a246aecbc17ff208d4e422f8738
vbenoot-itsme <vincent.benoot@itsme-id.com>
2026-03-04 17:34:01 +0100

    Cleans up some things in project files

--- wezterm/.config/wezterm/projects.lua
+++ wezterm/.config/wezterm/projects.lua
@@ -9,52 +9,14 @@ local function project_dirs()
 	local projects = {}
 
 	for _, dir in ipairs(wezterm.glob(project_dir .. "/*")) do
-		table.insert(projects, dir)
+		table.insert(projects, { label = dir })
 	end
 
 	return projects
 end
 
-function module.choose_project()
-	local choices = {}
-	for _, value in ipairs(project_dirs()) do
-		table.insert(choices, { label = value })
-	end
-
-	return wezterm.action.InputSelector({
-		title = "Projects",
-		choices = choices,
-		fuzzy = true,
-		action = wezterm.action_callback(function(child_window, child_pane, id, label)
-			if not label then
-				return
-			end
-
-			local existing_workspaces = mux.get_workspace_names()
-			local workspace_exists = false
-			for _, name in ipairs(existing_workspaces) do
-				if name == label then
-					workspace_exists = true
-					break
-				end
-			end
-
-			if not workspace_exists then
-				dev_workspace(child_window, child_pane, label, label)
-			end
-
-			child_window:perform_action(
-				wezterm.action.SwitchToWorkspace({
-					name = label,
-				}),
-				child_pane
-			)
-		end),
-	})
-end
-
-function dev_workspace(child_window, child_pane, cwd, label)
-	local tab, helix_pane, window = mux.spawn_window({
+local function dev_workspace(cwd, label)
+	local _, helix_pane, _ = mux.spawn_window({
 		workspace = label,
 		cwd = label,
 	})
@@ -74,5 +36,40 @@ function dev_workspace(child_window, child_pane, cwd, label)
 	helix_pane:send_text("hx .\n")
 	lazygit_pane:send_text("lazygit\n")
 end
+
+function module.choose_project()
+	return wezterm.action.InputSelector({
+		title = "Projects",
+		choices = project_dirs(),
+		fuzzy = true,
+		action = wezterm.action_callback(function(child_window, child_pane, _, label)
+			if not label then
+				return
+			end
+
+			local existing_workspaces = mux.get_workspace_names()
+			local workspace_exists = false
+			for _, name in ipairs(existing_workspaces) do
+				if name == label then
+					workspace_exists = true
+					break
+				end
+			end
+
+			if not workspace_exists then
+				dev_workspace(label, label)
+			end
+
+			child_window:perform_action(
+				wezterm.action.SwitchToWorkspace({
+					name = label,
+				}),
+				child_pane
+			)
+		end),
+	})
+end
+
+
 
 return module
